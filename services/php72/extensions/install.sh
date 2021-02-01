0168387e76a54bad8b4c19dfc835fe33afe5a108
pingo <pingstrong@163.com>
2021-02-01 17:43:33 +0800

    add

--- services/php72/extensions/install.sh
+++ services/php72/extensions/install.sh
@@ -579,7 +579,7 @@ if [[ -z "${EXTENSIONS##*,swoole,*}" ]]; then
 
     if [[ "$?" = "1" ]]; then
          
-        tgzName=swoole-4.6.1
+        tgzName=swoole-4.6.2
         extensionName="${tgzName%%-*}"
         mkdir ${extensionName}
         tar -xf ${tgzName}.tgz -C ${extensionName} --strip-components=1
